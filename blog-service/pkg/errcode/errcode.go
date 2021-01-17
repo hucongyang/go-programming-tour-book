db360cf332621317166f004b98f7fced9004ad4f
hucy <hucy@gegolab.com>
2021-01-17 12:34:09 +0800

    定义统一错误码返回格式

---
+++ blog-service/pkg/errcode/errcode.go
@@ -0,0 +1,76 @@
+package errcode
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Error struct {
+	code    int      `json:"code"`
+	msg     string   `json:"msg"`
+	details []string `json:"details"`
+}
+
+var codes = map[int]string{} // 作为全局错误码的存储载体
+
+func NewError(code int, msg string) *Error {
+	if _, ok := codes[code]; ok {
+		panic(fmt.Sprintf("错误码 %d 已经存在，请更换一个", code))
+	}
+	codes[code] = msg
+	return &Error{code: code, msg: msg}
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("错误码：%d, 错误信息：%s", e.Code(), e.Msg())
+}
+
+func (e *Error) Code() int {
+	return e.code
+}
+
+func (e *Error) Msg() string {
+	return e.msg
+}
+
+func (e *Error) Details() []string {
+	return e.details
+}
+
+func (e *Error) Msgf(args []interface{}) string {
+	return fmt.Sprintf(e.msg, args...)
+}
+
+func (e *Error) WithDetails(details ...string) *Error {
+	e.details = []string{}
+	for _, d := range details {
+		e.details = append(e.details, d)
+	}
+	return e
+}
+
+/**
+针对一些特定错误码进行状态码的转换
+*/
+func (e *Error) StatusCode() int {
+	switch e.Code() {
+	case Success.Code():
+		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
+	}
+
+	return http.StatusInternalServerError
+}
